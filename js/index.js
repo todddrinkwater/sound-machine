7ab9066d364ba530bea783498afc4ec48b2297d7
Todd Drinkwater <todd@fluxfederation.com>
2018-01-26 11:40:58 +1300

    keys change color on keypress and mouse click

--- js/index.js
+++ js/index.js
@@ -8,44 +8,53 @@ setButtons(drums)
 
 function setButtons(type){
   for (var i = 0; i < soundButtons.length; i++) {
+      j = 9 - i
+      var color = `#e${i}${j}a27`
+
       var soundButton = soundButtons[i];
 
       var header = document.createElement('h1')
       var title = document.createTextNode(type[i])
       var att = document.createAttribute("value")
+      var id = document.createAttribute("id")
 
       att.value = type[i]
+      id.value = type[i]
+
       soundButton.setAttributeNode(att)
+      soundButton.setAttributeNode(id)
+      
+      document.getElementById(id.value).style.backgroundColor = color
 
       var soundName = soundButton.getAttribute("value")
       var appendNew = soundButton.appendChild(header).appendChild(title)
 
-      soundName != undefined ? prepareButton(soundButton, soundName, keys[i]) : ''
+      soundName != undefined ? prepareButton(soundButton, soundName, keys[i], color) : ''
   }
 }
 
 
-function prepareButton(buttonEl, soundName, key) {
+function prepareButton(buttonEl, soundName, key, color){
     // buttonEl.querySelector('.').style.backgroundImage = 'url("img/icons/' + soundName + '.png")';
 
     var audio = new Audio(__dirname + '/wav/drums/' + soundName + '.wav');
     buttonEl.addEventListener('click', function () {
+        buttonEl.style.backgroundColor = "blue";
         audio.currentTime = 0;
         audio.play();
+        setTimeout(() => { buttonEl.style.backgroundColor = color }, 100)
     });
 
-    createKeyPressEvent(key, audio)
+    createKeyPressEvent(key, audio, color, buttonEl)
 }
 
-function createKeyPressEvent(setKey, audio){
+function createKeyPressEvent(setKey, audio, color, buttonEl){
   window.addEventListener("keydown", function (event) {
-    // if (event.defaultPrevented) {
-    //   return; // Do nothing if the event was already processed
-    // }
-
     if(event.key == setKey){
+      buttonEl.style.backgroundColor = "blue";
       audio.currentTime = 0;
       audio.play();
+      setTimeout(() => { buttonEl.style.backgroundColor = color }, 100)
     }
 
     // Cancel the default action to avoid it being handled twice
