d79dcefc7070edf9cd2ae4d886663d5cb6cb83d5
Todd Drinkwater <todd@fluxfederation.com>
2018-01-26 09:23:04 +1300

    add drum sounds

--- js/index.js
+++ js/index.js
@@ -1,26 +1,30 @@
 var soundButtons = document.querySelectorAll('.button')
 
-soundType = ['applause', 'ba-dum-tsss', 'burp', 'crowd-laughing', 'fart', 'money', 'sad-trombone']
+random = ['applause', 'ba-dum-tsss', 'burp', 'crowd-laughing', 'fart', 'money', 'sad-trombone']
+drums = ['bass', 'hi-hat', 'crash', 'snare', 'tom-1', 'tom-2']
 
-for (var i = 0; i < soundButtons.length; i++) {
-  // console.log(soundType[i]);
-    var soundButton = soundButtons[i];
+setButtons(drums)
 
-    var header = document.createElement('h1')
-    var title = document.createTextNode(soundType[i])
-    var att = document.createAttribute("value")
-    att.value = soundType[i]
-    soundButton.setAttributeNode(att)
-    var soundName = soundButton.getAttribute("value")
+function setButtons(type){
+  for (var i = 0; i < soundButtons.length; i++) {
+      var soundButton = soundButtons[i];
 
-    var appendNew = soundButton.appendChild(header).appendChild(title)
-    soundName != 'undefined' ? prepareButton(soundButton, soundName) : ''
+      var header = document.createElement('h1')
+      var title = document.createTextNode(type[i])
+      var att = document.createAttribute("value")
+      att.value = type[i]
+      soundButton.setAttributeNode(att)
+      var soundName = soundButton.getAttribute("value")
+
+      var appendNew = soundButton.appendChild(header).appendChild(title)
+      soundName != undefined ? prepareButton(soundButton, soundName) : ''
+  }
 }
 
 function prepareButton(buttonEl, soundName) {
     // buttonEl.querySelector('.').style.backgroundImage = 'url("img/icons/' + soundName + '.png")';
 
-    var audio = new Audio(__dirname + '/wav/' + soundName + '.wav');
+    var audio = new Audio(__dirname + '/wav/drums/' + soundName + '.wav');
     buttonEl.addEventListener('click', function () {
         audio.currentTime = 0;
         audio.play();
